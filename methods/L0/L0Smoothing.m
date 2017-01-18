15c7024ed3488c0ea735fbaca0acda41b8d3ce8c
sysuKinthon <qinkai1994@qq.com>
2017-01-18 11:49:58 +0800

    learning L0

--- methods/L0/L0Smoothing.m
+++ methods/L0/L0Smoothing.m
@@ -39,8 +39,9 @@ betamax = 1e5;
 fx = [1, -1];
 fy = [1; -1];
 [N,M,D] = size(Im);
+size(Im)
 sizeI2D = [N,M];
-otfFx = psf2otf(fx,sizeI2D);
+otfFx = psf2otf(fx,sizeI2D); %计算偏导函数的fft2
 otfFy = psf2otf(fy,sizeI2D);
 Normin1 = fft2(S);
 Denormin2 = abs(otfFx).^2 + abs(otfFy ).^2;
@@ -51,7 +52,7 @@ beta = 2*lambda;
 while beta < betamax
     Denormin   = 1 + beta*Denormin2;
     % h-v subproblem
-    h = [diff(S,1,2), S(:,1,:) - S(:,end,:)];
+    h = [diff(S,1,2), S(:,1,:) - S(:,end,:)]; %diff(S,1,2)表示S对x（列）求一阶导数；S(:,1,:)表示对S取所有行(:),第一列，所有深度(:)；这一步S(:,1,:) - S(:,end,:)的操作是将最后一列的值 设置为第一行减去最后一行。
     v = [diff(S,1,1); S(1,:,:) - S(end,:,:)];
     if D==1
         t = (h.^2+v.^2)<lambda/beta;
@@ -59,11 +60,12 @@ while beta < betamax
         t = sum((h.^2+v.^2),3)<lambda/beta;
         t = repmat(t,[1,1,D]);
     end
-    h(t)=0; v(t)=0;
+    h(t)=0; v(t)=0; %matlab的下标索引方式；这一步的操作是将符合条件的t的所有hp和vp的值都重新设置为0,而不满足的就是一开始的偏导值了。
+    %经过上面的操作，已经将各个位置p的h和v都求出来了
     % S subproblem
-    Normin2 = [h(:,end,:) - h(:, 1,:), -diff(h,1,2)];
-    Normin2 = Normin2 + [v(end,:,:) - v(1, :,:); -diff(v,1,1)];
-    FS = (Normin1 + beta*fft2(Normin2))./Denormin;
+    Normin2 = [h(:,end,:) - h(:, 1,:), -diff(h,1,2)]; %根据公式，先进行偏导，再进行fft2，也是可以的。对h进行x求导
+    Normin2 = Normin2 + [v(end,:,:) - v(1, :,:); -diff(v,1,1)];%对v进行y求导，也就是行。
+    FS = (Normin1 + beta*fft2(Normin2))./Denormin; %Normin1是对原图像的fft2; 
     S = real(ifft2(FS));
     beta = beta*kappa;
     fprintf('.');
