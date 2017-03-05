df52f6c648841098c3a4bc0ce13903eb1f2878e0
sysukinthon <qinkai1994@qq.com>
2017-03-05 14:58:47 +0800

    update

--- methods/L0/testL0Smoothing.m
+++ methods/L0/testL0Smoothing.m
@@ -2,5 +2,5 @@ Im = imread('pflower.jpg');
 S = L0Smoothing(Im,0.01);
 %f = fspecial('gaussian',[9,9],10);
 %img_smooth = imfilter(Im, f, 'same');
-figure, imshow(S);
+figure, imshow(cat(2, Im, im2uint8(S)));
 %figure, imshow(img_smooth);
