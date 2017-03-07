bb4932a73536d10a8f7b5737a9ca7f60efb2616c
sysukinthon <qinkai1994@qq.com>
2017-03-07 11:09:51 +0800

    update

--- TrainingCodes/deepepf_L0_Res_Bnorm_Adam/Demo_Train_model_L0_Res_Bnorm_Adam.m
+++ TrainingCodes/deepepf_L0_Res_Bnorm_Adam/Demo_Train_model_L0_Res_Bnorm_Adam.m
@@ -27,7 +27,7 @@ end
 %%%-------------------------------------------------------------------------
 opts.modelName        = model_dir_shape; %%% model name
 %opts.learningRate    = [logspace(-3,-3,30) logspace(-4,-4,20)];%%% you can change the learning rate
-opts.learningRate     = [logspace(-3,-3,20) logspace(-4,-4,28)];
+opts.learningRate     = [logspace(-3,-3,20) logspace(0,-3,10)];
 %opts.learningRate = [logspace(-3,-3,2)];
 opts.batchSize        = 128; %%% default
 opts.gpus             = [1]; %%% this code can only support one GPU!
