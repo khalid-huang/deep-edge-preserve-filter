97579c60bf6bfab90a5f599192904bcf626f69b8
sysuKinthon <qinkai1994@qq.com>
2017-03-10 13:52:03 +0800

    update

--- methods/wls/Demo_WLS_Filter.m
+++ methods/wls/Demo_WLS_Filter.m
@@ -6,41 +6,32 @@
 clc; close all;
 clear all;
 
-% WLS�˲�����
-lambda = 0.35;	% ��С�������򻯲����������ֵ���Բ�����ƽ����ͼ��
-alpha = 1.8;	% ���Ӹ�ֵ������Ϊ�񻯵ı�Ե
+lambda = 0.35;
+alpha = 1.8;
 
-% ��ȡͼ��
 img = double(imread('pflower.jpg'))./255;
 %img = rgb2gray(img);
-% ��ʾ����ͼ��
-figure, imshow(img),title('Input');
 
-% wΪͼ���ȣ�hΪͼ��߶ȣ�cΪ��ɫͨ��
+%figure, imshow(img),title('Input');
+
 [h, w, c] = size(img);
 
-%% ͼ����
-% �ж��ǲ�ɫͼ���ǻҶ�ͼ��
-if c == 1 % �Ҷ�ͼ��
-  
-    % ��Ȩ��С���˴���
-    outimg = wlsFilter(img, lambda, alpha); 
+if c == 1
 
-else  % ��ɫͼ��
-    
-    % ���ͼ��ռ�
+    outimg = wlsFilter(img, lambda, alpha);
+
+else
+
     outimg = img;
-    
-    % ʹ�ü�Ȩ��С�����˲����д���
+
     outimg(:,:,1) = wlsFilter(img(:,:,1), lambda, alpha);
     outimg(:,:,2) = wlsFilter(img(:,:,2), lambda, alpha);
     outimg(:,:,3) = wlsFilter(img(:,:,3), lambda, alpha);
-    
+
 end
 
-figure, imshow(outimg),title('Output');
+%figure, imshow(outimg),title('Output');
 % figure, imshow(outimg),colormap(Jet),title('Result Colorization');
 
 %outname = [outputpath,filename,'_WLS.png'];
-%imwrite(outimg, outname); % ���洦����
-
+%imwrite(outimg, outname)
---
+++ methods/wls/wls_run.m
@@ -0,0 +1,38 @@
+% ------------------------------------------------
+% Weighted Least Square Filter
+% Edge-preserving decompositions for multi-scale tone and detail_SG08
+% ------------------------------------------------
+ function [outimg] = wls_run(im_path)
+  %clc; close all;
+  %clear all;
+
+  lambda = 0.35;
+  alpha = 1.8;
+
+  img = double(imread(im_path))./255;
+  %img = rgb2gray(img);
+
+  %figure, imshow(img),title('Input');
+
+  [h, w, c] = size(img);
+
+  if c == 1
+
+      outimg = wlsFilter(img, lambda, alpha);
+
+  else
+
+      outimg = img;
+
+      outimg(:,:,1) = wlsFilter(img(:,:,1), lambda, alpha);
+      outimg(:,:,2) = wlsFilter(img(:,:,2), lambda, alpha);
+      outimg(:,:,3) = wlsFilter(img(:,:,3), lambda, alpha);
+
+  end
+
+  %figure, imshow(outimg),title('Output');
+  % figure, imshow(outimg),colormap(Jet),title('Result Colorization');
+
+  %outname = [outputpath,filename,'_WLS.png'];
+  %imwrite(outimg, outname)
+end
